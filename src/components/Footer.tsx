e3fd84415ca6f14a9a7499e8e8ae3ab06cfc4e16
Daria Kozlova <kozloffa.daria@gmail.com>
2025-12-15 15:37:18 +0100

    Footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 export const Footer =()=>{
   return (
-    <div>Footer</div>
+      <footer className="py-16 text-center text-sm opacity-70">
+        <p className="font-heading mb-2">Art&Fact</p>
+        <p>Discover. Collect. Reflect.</p>
+        <p className="mt-4">© 2025 · Art Institute of Chicago API</p>
+      </footer>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,13 +40,6 @@ export const Home = () => {
         </blockquote>
         <p className="mt-4">— Thomas Merton</p>
       </section>
-
-      {/* FOOTER */}
-      <footer className="py-16 text-center text-sm opacity-70">
-        <p className="font-heading mb-2">Art&Fact</p>
-        <p>Discover. Collect. Reflect.</p>
-        <p className="mt-4">© 2025 · Art Institute of Chicago API</p>
-      </footer>
     </div>
   );
 };
