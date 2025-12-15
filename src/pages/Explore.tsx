1a16073e5221119ef71f4ba3dc399f1f502d3a7a
Daria Kozlova <kozloffa.daria@gmail.com>
2025-12-15 16:50:07 +0100

    ArtworkSchema created. SearchAPI added

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,5 +1,12 @@
-export const Explore =()=>{
-  return (
-    <div>Explore</div>
-  )
-}
+import { useEffect } from "react";
+import { ArtworksService } from "../api/artInstitute";
+
+export const Explore = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      await ArtworksService.searchArtworks();
+    };
+    fetchData();
+  }, []);
+  return <div>Explore</div>;
+};
